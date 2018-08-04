2558bf4f80b17bdfbb07d49f05e3ee5320bcb51b
xuwk <1649464377@qq.com>
2018-08-04 12:45:12 +0800

    input file

--- updateScript/input.py
+++ updateScript/input.py
@@ -8,6 +8,7 @@ password = input("Password：")
 title = input("Title/标题：")
 host = input("Host/主办人（选填）：")
 speaker = input("Speaker/主讲人:")
+print("The format of the time is like this: 14:00—15:00, August 5(hh:mm-hh:mm, month(full name) day(int)")
 time = input("Time/时间：")
 location = input("Location/地点：")
 abstract = input("Abstract/简介：")
@@ -17,4 +18,10 @@ data1 = {"Title": title, "Host": host, "Speaker": speaker, "Time": time, "Locati
          "Abstract": abstract, "Bio": bio}
 data = parse.urlencode(data2).encode('utf-8')
 url = 'http://10.15.21.58:80/events/SIST?password='+password
-requests.post(url,data = json.dumps(data1))
+requests.post(url, data=json.dumps(data1))
+
+
+
+
+
+
