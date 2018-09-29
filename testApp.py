ab54b4a9a5bce910a22a8a2c346ada9eed0d2d15
Zhang Yifeng <zhangyf@shanghaitech.edu.cn>
2018-09-29 11:06:06 +0800

    modified testApp.py, removed port option in app.run()

--- testApp.py
+++ testApp.py
@@ -162,4 +162,4 @@ if __name__ == '__main__':
     handler = RotatingFileHandler('foo.log', maxBytes=10000, backupCount=1)
     handler.setLevel(logging.INFO)
     app.logger.addHandler(handler)
-    app.run(debug=debugBool, host="0.0.0.0", port=5000)
+    app.run(debug=debugBool, host="0.0.0.0")
