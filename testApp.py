ad2d2c00bc9c258ab1d651b18fc5f5af08970712
Zhang Yifeng <zhangyf@shanghaitech.edu.cn>
2018-10-05 00:17:29 +0800

    Another bug...

--- testApp.py
+++ testApp.py
@@ -25,15 +25,9 @@ app = Flask(__name__)
 api = Api(app)
 
 
-formatter = logging.Formatter('%(name)-12s %(asctime)s level-%(levelname)-8s thread-%(thread)-8d %(message)s')   # 每行日志的前缀设置
-log = logging.getLogger('api')
-fileTimeHandler = TimedRotatingFileHandler("logs/api_", "S", 1, 10)
-fileTimeHandler.suffix = "%Y%m%d.log"  #设置 切分后日志文件名的时间格式 默认 filename+"." + suffix 如果需要更改需要改logging 源码
-fileTimeHandler.setFormatter(formatter)
-logging.basicConfig(level = logging.INFO)
-fileTimeHandler.setFormatter(formatter)
-log.addHandler(fileTimeHandler)
-
+handler = RotatingFileHandler('log.log', maxBytes=10000, backupCount=1)
+handler.setLevel(logging.INFO)
+app.logger.addHandler(handler)
 
 
 class PopularEvents(Resource):
