ec50c2c7d7bf91bf8ba709165775f286fa7a87df
Zhang Yifeng <zhangyf@shanghaitech.edu.cn>
2018-10-21 14:38:27 +0800

    Deleted initSession.py

--- initSession.py
+++
@@ -1,8 +0,0 @@
-from sqlitedict import SqliteDict
-mydict = SqliteDict('kv.db', autocommit=True)
-mydict['some_key'] = ("session_key", "open_id")
-print (mydict['some_key'])  # prints the new value
-for key in mydict:
-    print(key, mydict[key])
-print(len(mydict))
-mydict.close()
